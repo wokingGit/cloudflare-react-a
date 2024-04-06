0a559a9a42698a60edd195a84d49e13ac4ca4d98
信春哥 <1575417080@qq.com>
2024-04-06 14:34:52 +0800

    444

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import Posts from './components/posts';
-import Post from './components/post';
+import Posts from "./components/posts";
+import Post from "./components/post";
 
 function App() {
+  let url = "https://cloudflare-worker-a.woking.workers.dev/";
+  fetch(url)
+    .then((response) => {
+      console.log("******",response);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => console.log(data))
+    .catch((error) =>
+      console.error("There was a problem with the fetch operation:", error)
+    );
+
   return (
     <Routes>
       <Route path="/" element={<Posts />} />
