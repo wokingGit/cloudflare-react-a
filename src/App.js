65a52d3f550b7f5694f363fd1cd467e7fefdab1b
信春哥 <1575417080@qq.com>
2024-04-10 00:18:48 +0800

    555

--- src/App.js
+++ src/App.js
@@ -5,24 +5,39 @@ import Post from "./components/post";
 
 function App() {
   let url = "https://cloudflare-worker-a.woking.workers.dev/";
-  fetch(url)
-    .then((response) => {
-      console.log("******",response);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((data) => console.log(data))
-    .catch((error) =>
-      console.error("There was a problem with the fetch operation:", error)
-    );
+  // fetch(url)
+  //   .then((response) => {
+  //     console.log("******", response);
+  //     if (!response.ok) {
+  //       throw new Error(`HTTP error! status: ${response.status}`);
+  //     }
+  //     return response.json();
+  //   })
+  //   .then((data) => console.log(data))
+  //   .catch((error) =>
+  //     console.error("There was a problem with the fetch operation:", error)
+  //   );
+
+  const cleckme = () => {
+    console.log(1111);
+    fetch("/api")
+      .then((data) => {
+        console.log(data);
+        return data.text();
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  };
 
   return (
-    <Routes>
-      <Route path="/" element={<Posts />} />
-      <Route path="/posts/:id" element={<Post />} />
-    </Routes>
+    <div>
+      <button onClick={cleckme}>点我..</button>
+    </div>
+    // <Routes>
+    //   <Route path="/" element={<Posts />} />
+    //   <Route path="/posts/:id" element={<Post />} />
+    // </Routes>
   );
 }
 
